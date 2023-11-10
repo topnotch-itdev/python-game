0e4819b58197cd51f934aca9eed26e7fa6b85990
PlanetGit <topnotchdev@outlook.com>
2023-11-10 21:18:35 +0900

    Implemented initial logic

---
+++ main.py
@@ -0,0 +1,29 @@
+from settings import *
+
+class Main:
+  def __init__(self):
+    # general
+    pygame.init()
+    self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    # game objects
+    self.bg_rects = [pygame.Rect((col + int(row % 2 == 0)) * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE) 
+                     for col in range(0, COLS, 2) for row in range(ROWS)]
+
+  def draw_bg(self):
+    for rect in self.bg_rects:
+      pygame.draw.rect(self.display_surface, 'red', rect)
+
+  def run(self):
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          exit()
+
+      self.display_surface.fill(LIGHT_GREEN)
+      self.draw_bg()
+      pygame.display.update()
+
+main = Main()
+main.run()
---
+++ settings.py
@@ -0,0 +1,21 @@
+import pygame
+from sys import exit
+from os.path import join
+
+CELL_SIZE = 50
+ROWS = 10
+COLS = 16
+WINDOW_WIDTH = COLS * CELL_SIZE
+WINDOW_HEIGHT = ROWS * CELL_SIZE
+
+# colours
+LIGHT_GREEN = '#aad751'
+DARK_GREEN = '#a2d149'
+
+# start pos
+START_LENGTH = 3
+START_ROW = ROWS // 2
+START_COL = START_LENGTH + 2
+
+SHADOW_SIZE = pygame.Vector2(4,4)
+SHADOW_OPACITY = 50

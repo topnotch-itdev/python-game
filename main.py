84284cfcfef66099eb57d0cabb433a42985eb13b
PlanetGit <topnotchdev@outlook.com>
2023-11-16 13:06:12 +0900

    Updated animation move

--- main.py
+++ main.py
@@ -16,10 +16,23 @@ class Main:
     self.snake = Snake()
     self.apple = Apple(self.snake)
 
+    # timer
+    self.update_event = pygame.event.custom_type()
+    pygame.time.set_timer(self.update_event, 200)
+
   def draw_bg(self):
     self.display_surface.fill(LIGHT_GREEN)
     for rect in self.bg_rects:
       pygame.draw.rect(self.display_surface, DARK_GREEN, rect)
+
+  def input(self):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]: self.snake.direction = pygame.Vector2(1, 0)
+    if keys[pygame.K_LEFT]: self.snake.direction = pygame.Vector2(-1, 0)
+    if keys[pygame.K_UP]: self.snake.direction = pygame.Vector2(0, -1)
+    if keys[pygame.K_DOWN]: self.snake.direction = pygame.Vector2(0, 1)
+
+    
 
   def run(self):
     while True:
@@ -27,7 +40,14 @@ class Main:
         if event.type == pygame.QUIT:
           pygame.quit()
           exit()
+        
+        if event.type == self.update_event:
+          self.snake.update()
+      
+      # updates
+      self.input()
 
+      # drwaing
       self.draw_bg()
       self.snake.draw()
       self.apple.draw()
--- snake.py
+++ snake.py
@@ -4,6 +4,16 @@ class Snake:
   def __init__(self):
     self.display_surface = pygame.display.get_surface()
     self.body = [pygame.Vector2(START_COL - col, START_ROW) for col in range(START_LENGTH)]
+    self.direction = pygame.Vector2(1, 0)
+
+  def update(self):
+    body_copy = self.body[:-1]
+    #1. get the head and move the head by the direction
+    new_head = body_copy[0] + self.direction
+    #2. insert the new head at index 0
+    body_copy.insert(0, new_head)
+    # update the original body
+    self.body = body_copy[:]
   
   def draw(self):
     for point in self.body:
